72a46742e6de4b8a0b02b4a2d8eb4058e2a2b7bb
Dennis Shakava <dennis.shakava@student.moringaschool.com>
2022-06-11 23:11:54 +0300

    commit tree 8237648b023c8235f544d6a5a7a369b2d052219a

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+const saturdayFun = function(activity="roller-skate", activity2="bathe my dog") {
+  if (activity){
+    return ( `This Saturday, I want to ${activity}!`)
+  }
+  else if (activity2) {
+    return ( `This Saturday, I want to ${activity2}!`)
+  }
+  
+}
+let mondayWork = function(activity="go to the office" , activity2= "work from home") {
+  if (activity) {
+    return (`This Monday, I will ${activity}.`)
+  }
+  else if (activity2) {
+
+    return (`This Monday, I will ${activity2}.`)
+  }
+}
+
+let wrapAdjective = function(style="*") {
+  return function (adjective ="special") {
+return `You are ${style}${adjective}${style}!`
+  }
+} 
